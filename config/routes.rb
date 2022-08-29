da6a61e797ae38e147ec32ff9198701d533b3f72
lemon0922 <ima.com0922@icloud.com>
2022-08-29 15:42:59 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tops#index'
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create, :edit, :update]
 end
