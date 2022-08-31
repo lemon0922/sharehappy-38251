9358fcd561de0479016c87ba4b95f0f57e121a48
lemon0922 <ima.com0922@icloud.com>
2022-09-01 00:01:12 +0900

    バリデーションの設定とアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: '半角英数字を使用してください' }
+
+  has_many :events
 end
