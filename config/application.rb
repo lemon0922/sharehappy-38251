6b0f4ab44af49833bbcbdabb023702cc3672b3a5
lemon0922 <ima.com0922@icloud.com>
2022-08-31 11:45:46 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Sharehappy38251
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.beginning_of_week = :sunday
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,5 +18,3 @@ module Sharehappy38251
     # the framework and any gems in your application.
   end
 end
-
-config.beginning_of_week = :sunday
