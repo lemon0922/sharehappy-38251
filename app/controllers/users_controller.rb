fb42c0e0d21608253e4257493c69152a74422ce8
lemon0922 <ima.com0922@icloud.com>
2022-09-06 18:55:47 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    user = User.find(params[:id])
-    if current_user.update(user_params)
-      redirect_to events_path
-    else
-      render :edit
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:nickname, :email, :password)
+    @user = current_user
   end
 end
