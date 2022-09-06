03918ce914a7429273f2953582aa2fbd72b0e633
lemon0922 <ima.com0922@icloud.com>
2022-09-06 16:14:23 +0900

    showアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
     if current_user.update(user_params)
       redirect_to events_path
     else
