5cefc516a928fd4399a282e1bcd92f162fb05691
lemon0922 <ima.com0922@icloud.com>
2022-09-07 17:08:42 +0900

    deviseのルーティングエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   root to: 'tops#index'
   resources :events
   resources :users, only: :show
+
+  devise_scope :user do
+    get 'signin' => 'devise_token_auth/sessions#new'
+    post 'signin' => 'devise_token_auth/sessions#create'
+    post 'signup' => 'users#create'
+    put 'update' => 'users#update'
+  end
+
 end
