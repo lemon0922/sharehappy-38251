46305425f149dc55e848286181da0c832a7f1ace
lemon0922 <ima.com0922@icloud.com>
2022-09-08 20:22:53 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(_resource)
@@ -10,6 +11,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'lemon' && password == '0000'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
