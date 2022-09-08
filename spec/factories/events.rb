8b20fab9338b2888cb9d0a2677e346c398bb85a4
lemon0922 <ima.com0922@icloud.com>
2022-09-08 17:34:02 +0900

    FactoryBotの修正

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event do
-    title          { 'テスト' }
-    start_time     {Faker::Date.forward(50)}
-    content        { 'テストする' }
+    title      { 'テスト' }
+    start_time {Faker::Date.forward(50)}
+    content    {Faker::Lorem.sentence}
   end
 end
