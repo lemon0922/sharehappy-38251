3fd51995d7089a3b45dd9ecbebab14e5476e511e
lemon0922 <ima.com0922@icloud.com>
2022-09-13 16:27:23 +0900

    クリエイトメソッドの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,14 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
 end
